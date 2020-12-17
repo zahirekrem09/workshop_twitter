b7f2b929163bf30775d5757ec04da1ec2d664dba
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-12-17 21:07:47 +0200

    Add icons Comp.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from "next/head";
-import SvgComponent from "../components/icon/Add";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <SvgComponent />
-    </div>
-  );
+  return <div className={styles.container}>Home</div>;
 }
